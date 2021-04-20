3dcd276d85d3e3369f6db05866d3805cb2593602
unknown <felipetubba@gmail.com>
2021-04-20 19:23:20 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5f62c8191fb4acf511000c83e985bdbe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd3719c41-6d76-4801-930b-b436fe7f13ec'
+};
